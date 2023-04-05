b0a09c45c594b615a77eb0ae5a7cf24df7410f8a
Stefani <sgancheva15@gmail.com>
2023-04-05 16:42:11 +0100

    Create task4.cs

---
+++ task4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // create Http client
+            HttpClient httpClient = new HttpClient();
+
+
+            // create Http POST data
+            string postData = "{\"queryField\":\"test query\"}";
+            HttpContent content = new StringContent(postData, System.Text.Encoding.UTF8, "application/json");
+
+
+            // create the Http request
+            string url = "https://cdemo.free.beeceptor.com";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+            request.Content = content;
+
+            try
+            {
+                // send the http request and get the response
+                HttpResponseMessage response = await httpClient.SendAsync(request);
+                //check status code
+                //int statusCode = (int)response.StatusCode;
+                //Console.WriteLine($"HTTP status code: {statusCode}");
+
+
+
+                // read the response as a string
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                // print response to the console
+                Console.WriteLine(responseContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" error: {ex.Message}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
