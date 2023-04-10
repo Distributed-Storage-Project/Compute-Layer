93ae1004f291a511a03d556cd75de6c2f9a83527
HannahBagtasos <hannahvistabagtasos@gmail.com>
2023-04-10 20:36:56 +0100

    response to compute layer test commit

--- task2/Controllers/HomeController.cs
+++ task2/Controllers/HomeController.cs
@@ -24,8 +24,9 @@ namespace ComputeLayer.Controllers
          * Rewrite query as "Logs| where Timestamp >= datetime(2015-08-22 05:00) and Timestamp < datetime(2015-08-22 06:00)| where Level == \"e\" and Service == \"Inferences.UnusualEvents_Main\"| project Level, Timestamp, Message| limit 10"
          * Then it sends a 200 response
          */
-        public IActionResult addJSON([FromBody] Query query)
+        public IActionResult addJSON([FromBody] Query query, [FromServices] QueryController queryController)
         {
+
             return Ok();
         }
     }
