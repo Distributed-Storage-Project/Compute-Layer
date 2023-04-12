b2b6458e54267f5b71a15b60613b11993efa8442
HannahBagtasos <hannahvistabagtasos@gmail.com>
2023-04-12 22:39:49 +0100

    Make HTTP request to Storage Layer

--- task2/Controllers/HomeController.cs
+++ task2/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace ComputeLayer.Controllers
 {
@@ -12,46 +13,51 @@ namespace ComputeLayer.Controllers
     public class HomeController : Controller
     {
 
-        private readonly QueryController _queryController;
-
-        public HomeController(QueryController queryController)
-        {
-            _queryController = queryController;
-        }
-
         [HttpPost]
-        public async Task<IActionResult> AddJSON([FromBody] Query query, [FromServices] QueryController queryController)
+        public async Task<IActionResult> AddJSON([FromBody] Query query)
         {
             try
             {
-                var data = await _queryController.GetQueryResult(query);
+                // From Task 1 : Convert Kusto query to SQL query
+                string sqlQuery = ConvertToSql(query);
 
-                return Ok(data);
+                // Make HTTP request to Storage Layer
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(new { query = sqlQuery }), Encoding.UTF8, "application/json");
+                    var response = await httpClient.PostAsync("https://localhost:7076/api/query", content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseData = await response.Content.ReadAsStringAsync();
+
+                        // Convert JSON response to list of rows
+                        //test the data 
+                        //get the data and convert it into a list 
+                        List<List<string>> rows = JsonConvert.DeserializeObject<List<List<string>>>(responseData);
+
+                        // Return data to user
+                        return Ok(rows);
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing your request.");
+                    }
+                }
             }
             catch (Exception ex)
             {
-
+                // Handle errors
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
 
-        /* Query for task:
-         * {
-         *  "query": "Logs | where Timestamp >= datetime(2016-10-22 05:00) | where Level == \"e\" | limit 19"
-         * }
-         * Sends 200 response
-         * 
-         * Multiple line query:
-         * Original query:
-         * Logs
-         * | where Timestamp >= datetime(2015-08-22 05:00) and Timestamp < datetime(2015-08-22 06:00)
-         * | where Level == "e" and Service == "Inferences.UnusualEvents_Main"
-         * | project Level, Timestamp, Message
-         * | limit 10 
-         * 
-         * Rewrite query as "Logs| where Timestamp >= datetime(2015-08-22 05:00) and Timestamp < datetime(2015-08-22 06:00)| where Level == \"e\" and Service == \"Inferences.UnusualEvents_Main\"| project Level, Timestamp, Message| limit 10"
-         * Then it sends a 200 response
-         */
+        private string ConvertToSql(Query query)
+        {
+            // TODO: Implement Kusto to SQL query conversion logic
+            return "SELECT * FROM <table-name>";
+        }
+
+
     }
 }
 
