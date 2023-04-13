8363fef9ceb16a57f2d0634654a464087d18b97c
JiayueChi <124182736+JiayueChi@users.noreply.github.com>
2023-04-13 01:53:58 +0100

    Add files via upload

---
+++ Compute/Compute/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Kusto.Data.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Http;
+
+namespace ComputeLayer.Controllers
+{
+    [ApiController]
+    [Route("/query")]
+    public class HomeController : ControllerBase
+    {
+        private readonly HttpClient _storageClient;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            _storageClient = httpClientFactory.CreateClient("StorageAPI");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> QueryAsync([FromBody] String query)
+        {
+            try
+            {
+                // Convert Kusto query to SQL
+                string sql = KustoToSqlConverter.Convert(query);
+
+                // Create HttpContent with the SQL query as plain text
+                HttpContent content = new StringContent(sql, System.Text.Encoding.UTF8, "text/plain");
+
+                // Send SQL query to Storage layer
+                var response = await _storageClient.PostAsync("/api/query", content);
+
+                // Check response status code and return result or error message
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsStringAsync();
+                    return Ok(result);
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync();
+                    return StatusCode((int)response.StatusCode, error);
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+    }
+}
---
+++ Compute/Compute/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ComputeLayer.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddHttpClient("StorageAPI", client =>
+{
+    client.BaseAddress = new Uri("https:localhost/8080"); //storage api address
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
