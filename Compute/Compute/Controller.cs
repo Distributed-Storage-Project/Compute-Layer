1f8c90f359b4bd0461ff384bdb6d7d5a0de1d2fc
JiayueChi <124182736+JiayueChi@users.noreply.github.com>
2023-04-16 01:29:17 +0100

    Add files via upload

--- Compute/Compute/Controller.cs
+++ Compute/Compute/Controller.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace ComputeLayer.Controllers
 {
     [ApiController]
-    [Route("api/query")]
+    [Route("api/home")]
     public class HomeController : ControllerBase
     {
         private readonly string _queryControllerUrl = "https://localhost:7076/api/query"; // Update with your QueryController endpoint URL
